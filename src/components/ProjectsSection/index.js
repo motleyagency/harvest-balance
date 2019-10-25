4176c8dd38cafdbbd1127fabeb9b519bc9ca5694
Fredrik Boström <fredrik@fredrikbostrom.net>
2019-10-25 14:41:45 +0300

    Cleanup

--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -3,20 +3,21 @@ import { projectBalance } from '../../util/harvestBalance';
 
 function ProjectsSection() {
   const [data, setData] = useState({});
+
   const handleSubmit = async (startDate, endDate) => {
     const balance = await projectBalance({
       startDate: '20191021',
       endDate: '20191025',
     });
     setData(balance);
-
-    console.log(balance);
   };
 
   return (
     <h1>
       Projects
-      <button onClick={() => handleSubmit()}>Click me</button>
+      <button type="button" onClick={() => handleSubmit()}>
+        Click me
+      </button>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </h1>
   );
