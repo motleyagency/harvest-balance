5361c01c1826a75bfc95e1fa7c80f71fe823565e
Fredrik Boström <fredrik@fredrikbostrom.net>
2019-10-25 14:40:53 +0300

    Add raw data to frontend

--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { projectBalance } from '../../util/harvestBalance';
 
 function ProjectsSection() {
-  return <h1>Projects</h1>;
+  const [data, setData] = useState({});
+  const handleSubmit = async (startDate, endDate) => {
+    const balance = await projectBalance({
+      startDate: '20191021',
+      endDate: '20191025',
+    });
+    setData(balance);
+
+    console.log(balance);
+  };
+
+  return (
+    <h1>
+      Projects
+      <button onClick={() => handleSubmit()}>Click me</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </h1>
+  );
 }
 
 export default ProjectsSection;
