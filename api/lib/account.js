ada6c32ec25de85cb8ce1e4f40dbf8149ece4e1b
Fredrik Boström <fredrik.bostrom@motley.fi>
2021-03-31 21:18:25 +0300

    Add getToken for harvest server side auth

--- api/lib/account.js
+++ api/lib/account.js
@@ -11,6 +11,22 @@ const getAccount = token =>
     },
   }).then(response => response.json());
 
+const getToken = code =>
+  fetch('https://id.getharvest.com/api/v2/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Harvest Balance (fredrik.bostrom@motley.fi)',
+    },
+    body: JSON.stringify({
+      code,
+      client_id: process.env.HARVEST_CLIENT_ID,
+      client_secret: process.env.HARVEST_SECRET,
+      grant_type: 'authorization_code',
+    }),
+  }).then(response => response.json());
+
 module.exports = {
   getAccount,
+  getToken,
 };
